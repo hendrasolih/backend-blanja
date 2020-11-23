5d6af5947a4548ebc79dd3314e52a84f895d6116
hendrasolih <hendra.solih.jp@gmail.com>
2020-11-23 22:45:23 +0700

    add routes for histories

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,10 +6,12 @@ const welcomeRouter = require("./welcome");
 const productsRouter = require("./products");
 const productRouter = require("./product");
 const searchRouter = require("./search");
+const historyRouter = require("./history");
 
 mainRouter.use("/", welcomeRouter); // localhost:8000
 mainRouter.use("/products", productsRouter); // localhost:8000/products
 mainRouter.use("/product", productRouter); // localhost:8000/product
 mainRouter.use("/search", searchRouter); // localhost:8000/search
+mainRouter.use("/history", historyRouter); // localhost:8000/history
 
 module.exports = mainRouter;
