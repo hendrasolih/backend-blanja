fe55240e2e4d801b9bf0f49468cce399a3824664
hendrasolih <hendra.solih.jp@gmail.com>
2021-01-26 09:16:46 +0700

    install socket io

--- server.js
+++ server.js
@@ -1,24 +1,38 @@
-const mysql = require('mysql');
-const app = require('./app');
+const mysql = require("mysql");
+const app = require("./app");
+const http = require("http");
+const socketio = require("socket.io");
+
+//socket io
+const server = http.createServer(app);
+const io = socketio(server).sockets;
+io.on("connection", (socket) => {
+  console.log("a user connected ...", socket.id);
+  socket.on("chat message", (msg) => {
+    console.log(msg);
+    io.emit("chat message", msg);
+  });
+});
+//socket io
 
 // Create connection
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'web_blanja'
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "web_blanja",
 });
 
 // Connect
 db.connect((err) => {
-    if(err){
-        throw err;
-    }
-    console.log('MySQL connected...');
+  if (err) {
+    throw err;
+  }
+  console.log("MySQL connected...");
 });
 
-const port =8000;
+const port = 8000;
 
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+server.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
